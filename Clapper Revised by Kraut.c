3b63583800cd7e3ef26aed5c7d7ddcffce63d2c7
CarltheCodingKing <31775911+CarltheCodingKing@users.noreply.github.com>
2017-10-26 08:57:51 -0400

    faKeClApPeR
    reeeeeee

--- Clapper Revised by Kraut.c
+++ Clapper Revised by Kraut.c
@@ -8,10 +8,11 @@ task main()
 
 	while(true)
 	{
+		displayTextLine(4,"clapcount: %d", clapcount);
+		displayTextLine(5,"sound: %d", SensorValue(sound));
 		if(SensorValue(sound) >= 90)
 		{
 			clapcount= clapcount + 1;
-			displayTextLine(4,"clapcount: %d", clapcount);
 			wait10Msec(100);
 
 			if(clapcount == 1)
@@ -20,32 +21,17 @@ task main()
 				wait10Msec(20);
 				motor[flipper]= 0;
 
-				if(clapcount == 2);
-				{
-					motor[flipper] = 50;
-					wait10Msec(20);
-					motor[flipper]= 0;
-
-					if(clapcount == 3)
-					{
-						motor[flipper] = -50;
-						wait10Msec(20);
-						motor[flipper]= 0;
-
-						if(clapcount == 4);
-						{
-							motor[flipper] = 50;
-							wait10Msec(20);
-							motor[flipper]= 0;
+     	if(clapcount == 2)
+			{
+				motor[flipper] = 50;
+				wait10Msec(20);
+				motor[flipper]= 0;
 
 
 
 
-
-						}
-					}
-				}
 			}
 		}
 	}
+}
 }
